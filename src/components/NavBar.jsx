661a6143f57a926ca5a3a79bd00198e312c20c7b
Tamnhhe <tamnhhe@gmail.com>
2024-07-09 21:55:17 +0700

    Update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,6 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import EventIcon from '@mui/icons-material/Event';
@@ -14,25 +13,8 @@ import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 const NavBar = () => {
-  const [state, setState] = React.useState({
-    left: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-  const list = (anchor) => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
+  const list = (
+    <Box sx={{ width: 250 }} role="presentation">
       <List>
         {['Home', 'Assignments', 'Upcoming slots', 'Read user guide', 'Contact Support', 'Frequently Asked Questions'].map((text, index) => {
           let icon;
@@ -70,27 +52,13 @@ const NavBar = () => {
   );
 
   return (
-    <div>
-      {['left'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer(anchor, true)}
-          >
-            <HomeIcon />
-          </IconButton>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </div>
+    <Drawer
+      variant="persistent"
+      anchor="left"
+      open={true}
+    >
+      {list}
+    </Drawer>
   );
 };
 
